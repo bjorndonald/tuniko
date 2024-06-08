c11413f50bcc8f4f0729df0b8142aeb9a9563515
bjorndonald <bjorndonaldb@yahoo.com>
2024-06-08 19:31:11 +0100

    fix: changed cache

--- src/actions/translation.ts
+++ src/actions/translation.ts
@@ -1,6 +1,6 @@
 export const getTranslationsByCorpusId = async (corpusid: string, page: number = 1, sortBy: string = "", search: string = "") => {
     const res = await fetch(process.env.SERVER_URI + "/translations/" + corpusid + `?page=${page}&limit=5&sort_by=${sortBy}&search=${search}`, {
-        cache: "reload"
+        cache: "no-cache",
     })
     // The return value is *not* serialized
     // You can return Date, Map, Set, etc.
@@ -15,7 +15,7 @@ export const getTranslationsByCorpusId = async (corpusid: string, page: number =
 
 export const TranslationIsChosen = async (tid: string) => {
     const res = await fetch(process.env.SERVER_URI + "/translation/" + tid+ "/chosen", {
-        cache: "reload"
+        cache: "no-cache",
     })
     // The return value is *not* serialized
     // You can return Date, Map, Set, etc.
