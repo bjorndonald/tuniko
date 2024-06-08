aa7a532fd1166b4ba9a187514d7fdadfa48bb1ab
bjorndonald <bjorndonaldb@yahoo.com>
2024-06-08 16:02:41 +0100

    fix: languages id

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,4 +1,4 @@
-export const ENGLISH_LANGUAGE_ID = "8e36d677-f6d7-457a-8a34-65cdbd92e6fc"
-export const ANONYMOUS_USER_ID = "5e3b7a22-df8f-42ca-8a6e-e5fc03b07972"
-export const ANONYMOUS_USER_EMAIL = "anonymous@gmail.com"
-export const ALL_LANGUAGE_ID = "3542228a-b0c6-47f6-85c1-378cbf70ef27"
+export const ENGLISH_LANGUAGE_ID = process.env.NEXT_PUBLIC_ENGLISH_LANGUAGE_ID
+export const ANONYMOUS_USER_ID = process.env.NEXT_PUBLIC_ANONYMOUS_USER_ID
+export const ANONYMOUS_USER_EMAIL = process.env.NEXT_PUBLIC_ANONYMOUS_USER_EMAIL
+export const ALL_LANGUAGE_ID = process.env.NEXT_PUBLIC_ALL_LANGUAGE_ID
