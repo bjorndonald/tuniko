307db34dacf8788b090f175b4a465b8def206b80
bjorndonald <bjorndonaldb@yahoo.com>
2024-07-03 11:36:12 +0100

    feat: new branch

--- src/drizzle/0002_silent_shriek.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "corpustexts" ALTER COLUMN "entrytype" SET DATA TYPE varchar;--> statement-breakpoint
-ALTER TABLE "translations" ADD COLUMN "upvotes" numeric;--> statement-breakpoint
-ALTER TABLE "translations" ADD COLUMN "downvotes" numeric;
--- src/drizzle/0004_tired_sasquatch.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE "settings" RENAME COLUMN "user" TO "actor";--> statement-breakpoint
-ALTER TABLE "settings" DROP CONSTRAINT "settings_user_users_id_fk";
---> statement-breakpoint
-DO $$ BEGIN
- ALTER TABLE "settings" ADD CONSTRAINT "settings_actor_users_id_fk" FOREIGN KEY ("actor") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
