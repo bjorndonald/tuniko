0a254deba33d7ab0e442176eb8fccc5ec0ea3f68
bjorndonald <bjorndonaldb@yahoo.com>
2024-07-26 22:09:35 +0100

    fix: opengraph issue

--- src/app/meta.tsx
+++ src/app/meta.tsx
@@ -53,6 +53,7 @@ export const Meta = () => {
       <meta property="og:type" content="wbsite" />
       <meta property="og:title" content="Tuniko| Help Us Translate" />
       <meta property="og:image" content="/site/opengraph-image.png" />
+      <meta property="og:description" content="his is a platform that allows users to help each other with translations for languages not supported digitally" />
       <meta name={"view-transition"} content={"same-origin"} />
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
